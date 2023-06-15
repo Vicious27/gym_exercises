24411988564b80929986380540857df8fa4592f4
Raul Garcia <punkvomit21@gmail.com>
2023-06-14 22:56:03 -0400

    style TextField and add search btn

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -10,13 +10,38 @@ const SearchExercises = () => {
         Awesome Exercises You Should Know
       </Typography>
       <Box position="relative" mb="72px">
-        <TextField height="76px"
+        <TextField 
+          sx={{
+            input: {
+              fontWeight: '700', 
+              border: 'none', 
+              borderRadius: '4px'
+              },
+              width: { lg: '800px', xs: '350px'},
+              backgroundColor: '#FFF',
+              borderRadius: '40px'
+          }}
+        height="76px"
         value=""
         onChange={(e) => {}}
         placeholder="Search Exercises"
         type="text">
 
         </TextField>
+        <Button className="search-btn"
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#FFF',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px'},
+            fontSize: { lg: '20px', xs: '14px'},
+            height: '56px',
+            position: 'absolute',
+            right: '0'
+          }}
+        >
+          Search
+        </Button>
 
       </Box>
     </Stack>
